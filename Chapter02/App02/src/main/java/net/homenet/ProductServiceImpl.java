5b1269e46b2caf6333065a04d677ce87130847fe
Lee Sangdae <mousesd@gmail.com>
2018-12-04 00:58:07 +0900

    App02, DI with Spring annotation

---
+++ Chapter02/App02/src/main/java/net/homenet/ProductServiceImpl.java
@@ -0,0 +1,25 @@
+package net.homenet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@SuppressWarnings("WeakerAccess")
+@Component
+public class ProductServiceImpl implements ProductService {
+    private final ProductDao productDao;
+
+    @Autowired
+    public ProductServiceImpl(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        productDao.addProduct(product);
+    }
+
+    @Override
+    public Product findByProductName(String name) {
+        return productDao.findByProductName(name);
+    }
+}

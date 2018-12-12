2b11b0e0a365cae3260e4ab9d49dd6fd6b797bf2
Lee Sangdae <mousesd@gmail.com>
2018-12-12 12:48:20 +0900

    Chapter4:App01, #1) Get the DataSource use a util schema

---
+++ Chapter04/App01/src/main/java/net/homenet/Main.java
@@ -0,0 +1,20 @@
+package net.homenet;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BasicDataSource dataSource = (BasicDataSource) context.getBean(DataSource.class);
+
+        System.out.println("DriverName: " + dataSource.getDriverClassName());
+        System.out.println("url: " + dataSource.getUrl());
+        System.out.println("username: " + dataSource.getUsername());
+        System.out.println("password: " + dataSource.getPassword());
+        System.out.println("maxPoolSize: " + dataSource.getMaxActive());
+    }
+}

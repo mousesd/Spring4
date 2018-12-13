c5a3581eb0e2e89ebb29e8cb561c57c783f18f5d
Lee Sangdae <mousesd@gmail.com>
2018-12-13 13:33:07 +0900

    Chapter4:App09, Use a Spring JDBC(Java configuration)

--- Chapter04/App09/src/main/java/net/homenet/Main.java
+++ Chapter04/App09/src/main/java/net/homenet/Main.java
@@ -4,6 +4,7 @@ import net.homenet.domain.Pet;
 import net.homenet.repository.OwnerDao;
 import net.homenet.repository.PetDao;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(PetConfiguration.class);
 
         PetDao petDao = context.getBean(PetDao.class);
         System.out.println("All pet count=" + petDao.getPetCount());

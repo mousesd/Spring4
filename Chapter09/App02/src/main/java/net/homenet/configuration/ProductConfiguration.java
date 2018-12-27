ec3a8b84a040f19ee8c564fac670a2224925866b
Lee Sangdae <mousesd@gmail.com>
2018-12-27 15:35:39 +0900

    Chapter09:App02: Spring Cache with Java configuration

--- Chapter09/App02/src/main/java/net/homenet/configuration/ProductConfiguration.java
+++ Chapter09/App02/src/main/java/net/homenet/configuration/ProductConfiguration.java
@@ -4,10 +4,16 @@ import net.homenet.repository.ProductDao;
 import net.homenet.repository.ProductDaoImpl;
 import net.homenet.service.ProductService;
 import net.homenet.service.ProductServiceImpl;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheFactoryBean;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+
 @Configuration
+@EnableCaching
 public class ProductConfiguration {
     @Bean
     public ProductDao productDao() {
@@ -17,5 +23,19 @@ public class ProductConfiguration {
     @Bean
     public ProductService productService(ProductDao productDao) {
         return new ProductServiceImpl(productDao);
+    }
+
+    @Bean
+    public ConcurrentMapCacheFactoryBean concurrentMapCacheFactory() {
+        ConcurrentMapCacheFactoryBean concurrentMapCacheFactory = new ConcurrentMapCacheFactoryBean();
+        concurrentMapCacheFactory.setName("area");
+        return concurrentMapCacheFactory;
+    }
+
+    @Bean
+    public SimpleCacheManager cacheManager(ConcurrentMapCacheFactoryBean cache) {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(Collections.singletonList(cache.getObject()));
+        return cacheManager;
     }
 }
